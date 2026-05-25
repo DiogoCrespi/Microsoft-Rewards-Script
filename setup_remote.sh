3779f9744c9b6ec2bcdbdf486f7707e12f9b6d7e
diogocrespi <diogocrespi@users.noreply.github.com>
2026-05-25 08:58:39 -0300

    feat: implement Workers class to handle reward activities and remove obsolete shell scripts

--- setup_remote.sh
+++ setup_remote.sh
@@ -1,5 +1,5 @@
 cd Microsoft-Rewards-Script
-echo '[{"email":"diogocrespi@hotmail.com","password":"DiogoVinicioscrespi12@","totp":"","geoLocale":"auto","proxy":{"proxyAxios":true,"url":"","port":0,"username":"","password":""}}]' > src/accounts.json
+echo '[{"email":"[EMAIL_ADDRESS]","password":"[PASSWORD]","totp":"","geoLocale":"auto","proxy":{"proxyAxios":true,"url":"","port":0,"username":"","password":""}}]' > src/accounts.json
 cp src/config.example.json src/config.json
 sed -i "s/headless: this.bot.config.headless,/executablePath: '\/usr\/bin\/chromium', headless: this.bot.config.headless,/" src/browser/Browser.ts
 npm ci
