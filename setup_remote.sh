be9f334618d3d8fab3b5da1a5e802053826c4cd7
diogocrespi <diogocrespi@users.noreply.github.com>
2026-05-25 08:59:42 -0300

    chore: remove setup_remote.sh

--- setup_remote.sh
+++
@@ -1,6 +0,0 @@
-cd Microsoft-Rewards-Script
-echo '[{"email":"[EMAIL_ADDRESS]","password":"[PASSWORD]","totp":"","geoLocale":"auto","proxy":{"proxyAxios":true,"url":"","port":0,"username":"","password":""}}]' > src/accounts.json
-cp src/config.example.json src/config.json
-sed -i "s/headless: this.bot.config.headless,/executablePath: '\/usr\/bin\/chromium', headless: this.bot.config.headless,/" src/browser/Browser.ts
-npm ci
-npm run build
